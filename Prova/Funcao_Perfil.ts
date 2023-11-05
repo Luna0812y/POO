b0a25b922377ccd70ad6dd165176cb2d3f01eede
Luna <102774024+Luna0812y@users.noreply.github.com>
2023-11-05 12:20:13 -0300

    Update Funcao_Perfil.ts

--- Prova/Funcao_Perfil.ts
+++ Prova/Funcao_Perfil.ts
@@ -1,104 +1,97 @@
-// 01 - Criar as classes
-export class Perfil{ // a) Perfil
-    private _id : number
-    private _nome : string
-    private _email : string
-    private _postagens : Postagem[] = []
+// 01 - Criar as export export classes
+export class Perfil {
+    private _id: number;
+    private _nome: string;
+    private _email: string;
+    private _postagens: Postagem[] = [];
 
-   constructor (id : number, nome : string, email: string){
-        this._id = id
-        this._nome = nome
-        this._email = email
-   }
-    
-    get id() : number{
-        return this._id
+    constructor(id: number, nome: string, email: string) {
+        this._id = id;
+        this._nome = nome;
+        this._email = email;
     }
 
-    get nome() : string{
-        return this._nome
+    get id(): number {
+        return this._id;
     }
 
-    get email() : string{    
-        return this._email
+    get nome(): string {
+        return this._nome;
     }
 
-    get postagens(): Postagem[]{
-        return this._postagens
+    get email(): string {
+        return this._email;
+    }
+
+    get postagens(): Postagem[] {
+        return this._postagens;
     }
 
     adicionarPostagem(postagem: Postagem): void {
         this._postagens.push(postagem);
-    }    
-
+    }
 }
 
-export class Postagem { // b) Postagem
-    private _id : number
-    private _texto : string
-    private _curtidas : number
-    private _descurtidas : number
-    private _data : Date
-    private _perfil : Perfil
+export class Postagem {
+    private _id: number;
+    private _texto: string;
+    private _curtidas: number;
+    private _descurtidas: number;
+    private _data: Date;
+    private _perfil: Perfil;
 
-    constructor (id : number, texto : string, data : Date,  perfil : Perfil){
-            //falta o super
-        this._id = id
-        this._texto = texto
-        this._curtidas = 0
-        this._descurtidas = 0
-        this._data = data
-        this._perfil = perfil
+    constructor(id: number, texto: string, perfil: Perfil, data: Date) {
+        this._id = id;
+        this._texto = texto;
+        this._curtidas = 0;
+        this._descurtidas = 0;
+        this._perfil = perfil;
+        this._data = data;
     }
 
-    get id() : number{
-        return this._id
-    }
-    
-    get texto() : string{        
-        return this._texto
-    }
-    
-    get curtidas() : number{
-        return this._curtidas
+    get id(): number {
+        return this._id;
     }
 
-    get descurtidas() : number{
-        return this._descurtidas
+    get texto(): string {
+        return this._texto;
     }
 
-    get data() : Date{
-        return this._data
+    get curtidas(): number {
+        return this._curtidas;
     }
 
-    get perfil() : Perfil{
-        return this._perfil
+    get descurtidas(): number {
+        return this._descurtidas;
     }
 
-
-    // 2) Criar os métodos
-    //a)
-    curtir() : void{
-        this._curtidas++
+    get data(): Date {
+        return this._data;
     }
 
-    descurtir() : void{
-        this._descurtidas++
+    get perfil(): Perfil {
+        return this._perfil;
+    }
+
+    curtir(): void {
+        this._curtidas++;
+    }
+
+    descurtir(): void {
+        this._descurtidas++;
     }
 
     ehPopular(): boolean {
         return this._curtidas >= 1.5 * this._descurtidas;
     }
-    
 }
 
 export class PostagemAvancada extends Postagem {
     private _hashtags: string[];
     private _visualizacoesRestantes: number;
 
-    constructor(id: number, texto: string, perfil: Perfil, data: Date, hashtags: string[], 
-        visualizacoesRestantes: number) {
-        super(id, texto, data, perfil);
+    constructor(id: number, texto: string, perfil: Perfil, hashtags: string[], visualizacoesRestantes: number, data: Date) {
+        super(id, texto, perfil, data);
         this._hashtags = hashtags;
         this._visualizacoesRestantes = visualizacoesRestantes;
     }
@@ -107,13 +100,12 @@ export class PostagemAvancada extends Postagem {
         return this._hashtags;
     }
 
-    get visualizacoesRestantes(): number{
-        return this._visualizacoesRestantes
+    get visualizacoesRestantes(): number {
+        return this._visualizacoesRestantes;
     }
 
-    
     adicionarHashtag(hashtag: string): void {
-        this._hashtags[this._hashtags.length] = hashtag;
+        this._hashtags.push(hashtag);
     }
 
     existeHashtag(hashtag: string): boolean {
@@ -124,67 +116,48 @@ export class PostagemAvancada extends Postagem {
         }
         return false;
     }
-    
+
     decrementarVisualizacoes(): void {
         this._visualizacoesRestantes--;
     }
 }
 
-// 3) CLASSE REPOSITORIO DE PERFIS
 export class RepositorioDePerfis {
     private _perfis: Perfil[] = [];
 
     incluir(perfil: Perfil): void {
-        const perfilExistente = this.consultar(perfil.id, perfil.nome, perfil.email);
-        if (perfilExistente) {
-            console.log("Já existe um perfil com o mesmo ID, nome ou e-mail.");
-            return;
-        }
         this._perfis.push(perfil);
     }
-    
-  
-    /* é usando o operador "?" por uma maneira de indicar que o parâmetro não é 
-    obrigatório e pode ser omitido na chamada da função, ou seja, ele pode ser undefined.*/
+
     consultar(id?: number, nome?: string, email?: string): Perfil | null {
-    /*verifica se pelo menos um dos parâmetros de busca foi fornecido e, em seguida, 
-    verifica se há um perfil no array this._perfis que corresponde a todos os critérios 
-    de busca. Se encontrar um perfil correspondente, ele é retornado; caso contrário, o
-    método retorna null */
-        if (id !== undefined || nome !== undefined || email !== undefined) {
-            for (const perfil of this._perfis) {
-                const idMatch = id === undefined || perfil.id === id;
-                const nomeMatch = nome === undefined || perfil.nome === nome;
-                const emailMatch = email === undefined || perfil.email === email;
-    
-                if (idMatch && nomeMatch && emailMatch) {
-                    return perfil;
+            if (id !== undefined || nome !== undefined || email !== undefined) {
+                for (const perfil of this._perfis) {
+                    const idMatch = id === undefined || perfil.id === id;
+                    const nomeMatch = nome === undefined || perfil.nome === nome;
+                    const emailMatch = email === undefined || perfil.email === email;
+        
+                    if (idMatch && nomeMatch && emailMatch) {
+                        return perfil;
+                    }
                 }
             }
+            return null;
         }
-        return null;
-    }
-    
+        
 }
-  
-// 4) criar a classe RepositorioDePostagens
+
+
 export class RepositorioDePostagens {
     private _postagens: Postagem[] = [];
 
     incluir(postagem: Postagem): void {
-        const postagemExistente = this.consultar(postagem.id);
-        if (postagemExistente) {
-            console.log("Já existe uma postagem com o mesmo ID.");
-            return;
-        }
         this._postagens.push(postagem);
+        const perfil = postagem.perfil;
+        if (perfil) {
+            perfil.adicionarPostagem(postagem);
+        }
     }
-    
 
-    /*filtra postagens com base em critérios como id, texto, hashtag e perfil. Se nenhum 
-    critério for dado, retorna todas as postagens. Usa filter() para verificar 
-    correspondências e retorna um array das postagens que atendem aos 
-    critérios fornecidos. */
     consultar(id?: number, texto?: string, hashtag?: string, perfil?: Perfil): Postagem[] {
         if (id === undefined && texto === undefined && hashtag === undefined && perfil === undefined) {
             return this._postagens; // Retorna todas as postagens se nenhum critério é fornecido.
@@ -208,10 +181,8 @@ export class RepositorioDePostagens {
     
             return atendeCriterios;
         });
-    }    
-    
-    /* filtrar postagens com base em um fragmento específico de texto 
-    fornecido como critério de consulta. */
+    } 
+
     verificarSubstring(substring: string, texto: string): boolean {
         for (let i = 0; i <= texto.length - substring.length; i++) {
             if (texto.substring(i, i + substring.length) === substring) {
@@ -220,68 +191,5 @@ export class RepositorioDePostagens {
         }
         return false;
     }
-    
+
 }
-
-
-/*
-//imprimir
-// Criando perfis
-const perfil1 = new Perfil(1, "Alice", "alice@example.com");
-const perfil2 = new Perfil(2, "Bob", "bob@example.com");
-const perfil3 = new Perfil(3, "Charlie", "charlie@example.com");
-
-// Criando postagens
-const postagem1 = new Postagem(1, 10, "Texto da postagem 1", 2, perfil1, new Date());
-const postagem2 = new PostagemAvancada(2, 15, "Texto da postagem 2", 3, perfil2, new Date(), ["#hash1", "#hash2"], 100);
-const postagem3 = new Postagem(3, 5, "Texto da postagem 3", 1, perfil3, new Date());
-
-// Criando repositório de perfis e adicionando perfis
-const repositorioPerfis = new RepositorioDePerfis();
-repositorioPerfis.incluir(perfil1);
-repositorioPerfis.incluir(perfil2);
-repositorioPerfis.incluir(perfil3);
-
-// Criando repositório de postagens e adicionando postagens
-const repositorioPostagens = new RepositorioDePostagens();
-repositorioPostagens.incluir(postagem1);
-repositorioPostagens.incluir(postagem2);
-repositorioPostagens.incluir(postagem3);
-
-// Exemplos de consultas
-console.log("Exemplo 1: Todas as postagens:");
-const todasAsPostagens = repositorioPostagens.consultar();
-imprimirPostagens(todasAsPostagens);
-
-console.log("Exemplo 3: Consultar postagens por texto:");
-const postagensPorTexto = repositorioPostagens.consultar(undefined, "Texto da postagem 1");
-imprimirPostagens(postagensPorTexto);
-
-console.log("Exemplo 4: Consultar postagens por hashtag:");
-const postagensPorHashtag = repositorioPostagens.consultar(undefined, undefined, "#hash1");
-imprimirPostagens(postagensPorHashtag);
-
-console.log("Exemplo 5: Consultar postagens por perfil:");
-const postagensPorPerfil = repositorioPostagens.consultar(undefined, undefined, undefined, perfil3);
-imprimirPostagens(postagensPorPerfil);
-
-function imprimirPostagens(postagens: Postagem[]): void {
-    for (const postagem of postagens) {
-        imprimirPostagem(postagem);
-    }
-}
-
-function imprimirPostagem(postagem: Postagem): void {
-    console.log(`ID: ${postagem.id}`);
-    console.log(`Curtidas: ${postagem.curtidas}`);
-    console.log(`Texto: ${postagem.texto}`);
-    console.log(`Descurtidas: ${postagem.descurtidas}`);
-    console.log(`Perfil: ID ${postagem.perfil.id}, Nome: ${postagem.perfil.nome}, Email: ${postagem.perfil.email}`);
-    console.log(`Data: ${postagem.data}`);
-    if (postagem instanceof PostagemAvancada) {
-        console.log(`Hashtags: ${postagem.hashtags.join(', ')}`);
-        console.log(`Visualizações Restantes: ${postagem.visualizacoesRestantes}`);
-    }
-    console.log("------");
-}
-*/
